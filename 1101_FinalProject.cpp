f903246c3fcf154845363f75b147ab9593767b26
an-dr3w <mobikerfive@mobikerfive@gmail.com>
2020-11-23 14:17:41 -0600

    Created two functions numberBoard() and moveCheck()
    Added int array to main()
    
    Called numberBoard() to main()

--- 1101_FinalProject.cpp
+++ 1101_FinalProject.cpp
@@ -7,9 +7,36 @@
 using namespace std;
 
 
+// Function Prototypes
+void numberBoard(int);  // This function assigns the array a number for every place on the board
+void moveCheck(int);  // This function checks to see if the players move is an acceptable one between (1-9)
 
 int main()
 {
+    int arraylist[3][3];
+    numberBoard(arraylist);
+
     
 }
 
+void numberBoard(int arr[3][3]) 
+{
+    int i = 0, x = 0, y = 0;
+
+    for (i , i < 9; i++;) 
+    {
+        if (x == 2) {
+            x = 0;
+            y++;
+        }
+        arr[x][y] = i + 1;
+        x++;
+    }
+
+}
+
+void moveCheck(int arr) 
+{
+    
+
+}
