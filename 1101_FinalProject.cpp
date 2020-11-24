9d6530ae39fa7bb311cacbd497b5c31d96343005
an-dr3w <mobikerfive@mobikerfive@gmail.com>
2020-11-24 10:42:20 -0600

    created playerAnnouncement() function with greeting and strings for players to input their names. created argument to prompt player1 or 2's turn
    set counter in main to add 1 every turn

--- 1101_FinalProject.cpp
+++ 1101_FinalProject.cpp
@@ -5,19 +5,26 @@
 // we can use namespace just for now and add the "std::" to all the i/o commands after
 using namespace std;
 
-// Global variables
+// Global variable
 const int MAX_SIZE = 100;
 
 // Function Prototypes
 void numberBoard(int arr[][3]);  // This function assigns the array a number for every place on the board 
-void placeCheck(int playerMove, int moves);  // This function checks to see if the players move is between (1-9) and checks to see if there is a piece already on a spot 
+void playerAnnouncement(int moves, string& player1, string& player2);  // Greeting at the begining of the game, announces player1's turn and player2's turn 
+void placeCheck(int playerMove, int moves);  // This function checks to see if player input is between (1-9) and to see if there is a piece already on the spot 
 
 
 int main()
 {
-    int arraylist[3][3], playerMove = 0, i = 0, counter = 0;
-   
-    numberBoard(arraylist);
+    int gameBoard[3][3], playerMove = 0, i = 0, counter = 0;
+    string player1, player2;
+    
+    // Called function to number the game board 
+    numberBoard(gameBoard);
+
+    ++counter;
+    playerAnnouncement(counter, player1, player2);
+    cin >> playerMove;
     placeCheck(playerMove, counter);
     
 
@@ -39,17 +46,42 @@ void numberBoard(int arr[][3])
 }
 
 
+void playerAnnouncement(int moves, string& player1, string& player2) 
+{
+    if (moves == 1) 
+    {
+        cout << "Welcome, to tic-tac-toe!\n" << "Player1 enter your name: ";
+        cin >> player1;
+        cout << "\nPlayer2 enter your name: ";
+        cin >> player2;
+        cout << "\nThank you, lets get started.\n";
+    }
+
+    if (moves % 2 == 1) 
+    {
+        cout << endl << player1 << "'s move: ";
+    }
+    if (moves % 2 == 0) 
+    {
+        cout << endl << player2 << "'s move: ";
+    }
+
+}
+
+
 void placeCheck(int playerMove, int moves) 
 {
     int i = 0, count = 0, arr[MAX_SIZE];
     arr[moves] = playerMove;
-    
+
+    // checking if player input is within the allowed interval of [1-9] 
     if (playerMove < 1 || playerMove > 9)
     {
         cout << "The spot you choose does not exist\n";
     }
 
-    for (i, i < MAX_SIZE; i++;) 
+    // going through the list of previous moves to determine if the spot is already taken 
+    for (i, i < moves; i++;) 
     {
         if (playerMove == arr[i]) 
         {
