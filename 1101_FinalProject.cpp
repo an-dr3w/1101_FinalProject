49ce2cc424cd568c3dcc86329ec33f12bffdd49e
an-dr3w <mobikerfive@mobikerfive@gmail.com>
2020-12-01 16:46:14 -0600

    Making final visual adjustments to board on console

--- 1101_FinalProject.cpp
+++ 1101_FinalProject.cpp
@@ -17,24 +17,28 @@ int winnerCheck(char arr[][3], int counter, string player1, string player2);
 
 int main()
 {
+    // Declaring variables. 
     char gameBoard[3][3];
     int  winner = 0, counter = 1, row, column;
     string player1, player2;
     bool turn = 0;
     
-    // Called function to number the game board 
+
     numberBoard(gameBoard);
    
     do {
-      
+
         playerAnnouncement(counter, player1, player2);
         
-        cout << gameBoard[0][0] << " | " << gameBoard[0][1] << " | " << gameBoard[0][2] << endl;
-        cout << "__________" << endl;
-        cout << gameBoard[1][0] << " | " << gameBoard[1][1] << " | " << gameBoard[1][2] << endl;
-        cout << "__________" << endl;
-        cout << gameBoard[2][0] << " | " << gameBoard[2][1] << " | " << gameBoard[2][2] << endl;
+        cout << "           Columns" << endl << endl;
+        cout << "           0 " << "  1 " << "  2 " << endl;
+        cout << "       0   " << gameBoard[0][0] << " | " << gameBoard[0][1] << " | " << gameBoard[0][2] << endl;
+        cout << "           __________" << endl;
+        cout << " Rows  1   " << gameBoard[1][0] << " | " << gameBoard[1][1] << " | " << gameBoard[1][2] << endl;
+        cout << "           __________" << endl;
+        cout << "       2   " << gameBoard[2][0] << " | " << gameBoard[2][1] << " | " << gameBoard[2][2] << endl << endl;
         
+
         if (counter % 2 == 1)
         {
             do 
@@ -64,7 +68,9 @@ int main()
             } while (turn == 0);
             turn = 0;
         }
+        // Clears the screen so users don't have to scroll on the console. 
         system("cls");
+        // Declaring the returned value from function to winner 
         winner = winnerCheck(gameBoard, counter, player1, player2);
         counter++;
 
@@ -74,8 +80,10 @@ int main()
 }
 
 
+
 void numberBoard(char arr[][3]) 
 {
+    // Declaring each part of the array. 
     arr[0][0] = ' ';
     arr[0][1] = ' ';
     arr[0][2] = ' ';
@@ -88,8 +96,10 @@ void numberBoard(char arr[][3])
 }
 
 
+
 void playerAnnouncement(int moves, string& player1, string& player2) 
 {
+    // Welcoming announcement before first move. 
     if (moves == 1) 
     {
         cout << "Welcome, to tic-tac-toe!\n" << "Player1 enter your name: ";
@@ -99,6 +109,7 @@ void playerAnnouncement(int moves, string& player1, string& player2)
         cout << "\nThank you, lets get started.\n\n";
     }
 
+    // Announces the player that is next to make a move. 
     if (moves < 10) 
     {
         if (moves % 2 == 1)
@@ -113,6 +124,7 @@ void playerAnnouncement(int moves, string& player1, string& player2)
 }
 
 
+
 void playerMove(int& row, int& column)
 {
     cout << "Pick a row between [0-2]: ";
@@ -123,9 +135,10 @@ void playerMove(int& row, int& column)
 }
 
 
+
 void placeCheck(int row, int column) 
 {
-    // checking if player input is within the allowed board spaces 
+    // checking if player input is within the allowed board spaces. 
     if (row < 0 && row > 2 || column < 0 && column > 2)
     {
         cout << "The spot you choose does not exist\n";
@@ -133,11 +146,12 @@ void placeCheck(int row, int column)
 }
 
 
+
 int winnerCheck(char arr[][3], int counter, string player1, string player2) 
 {
     int winner = 0;
 
-    // Player1 winning argument 
+    // Players possible winning arguments that give winner a value depending on which char was on the spaces. 
     if (arr[0][0] == arr[0][1] && arr[0][0] == arr[0][2])
     {
         if (arr[0][0] == 'X')
@@ -234,7 +248,7 @@ int winnerCheck(char arr[][3], int counter, string player1, string player2)
         }
     }
    
-    // Tied game argument and picking a winner 
+    // Tied / Winning announcement with the final board. 
     if(counter == 9)
     {
         if (winner == 0) 
@@ -272,121 +286,3 @@ int winnerCheck(char arr[][3], int counter, string player1, string player2)
 }
 
 
-
-
-
-
-
-
-
-/*
-if (arr[0][0] == 'X' && arr[0][1] == 'X' && arr[0][2] == 'X')
-{
-    winner == true;
-}
-else if (arr[1][0] == 'X' && arr[1][1] == 'X' && arr[1][2] == 'X')
-{
-    winner == true;
-}
-else if (arr[2][0] == 'X' && arr[2][1] == 'X' && arr[2][2] == 'X')
-{
-    winner == true;
-}
-else if (arr[0][0] == 'X' && arr[1][0] == 'X' && arr[2][0] == 'X')
-{
-    winner == true;
-}
-else if (arr[0][1] == 'X' && arr[1][1] == 'X' && arr[2][1] == 'X')
-{
-    winner == true;
-}
-else if (arr[0][2] == 'X' && arr[1][2] == 'X' && arr[2][2] == 'X')
-{
-    winner == true;
-}
-else if (arr[0][0] == 'X' && arr[1][1] == 'X' && arr[2][2] == 'X')
-{
-    winner == true;
-}
-else if (arr[0][2] == 'X' && arr[1][1] == 'X' && arr[2][0] == 'X')
-{
-    winner == true;
-}
-else
-winner == false;
-
-// Player2 winning argument 
-if (arr[0][0] == 'O' && arr[0][1] == 'O' && arr[0][2] == 'O')
-{
-    winner == true;
-}
-else if (arr[1][0] == 'O' && arr[1][1] == 'O' && arr[1][2] == 'O')
-{
-    winner == true;
-}
-else if (arr[2][0] == 'O' && arr[2][1] == 'O' && arr[2][2] == 'O')
-{
-    winner == true;
-}
-else if (arr[0][0] == 'O' && arr[1][0] == 'O' && arr[2][0] == 'O')
-{
-    winner == true;
-}
-else if (arr[0][1] == 'O' && arr[1][1] == 'O' && arr[2][1] == 'O')
-{
-    winner == true;
-}
-else if (arr[0][2] == 'O' && arr[1][2] == 'O' && arr[2][2] == 'O')
-{
-    winner == true;
-}
-else if (arr[0][0] == 'O' && arr[1][1] == 'O' && arr[2][2] == 'O')
-{
-    winner == true;
-}
-else if (arr[0][2] == 'O' && arr[1][1] == 'O' && arr[2][0] == 'O')
-{
-    winner == true;
-}
-else
-winner == false;
-
-*/
-
-/*
-if (counter == 9)
-{
-    if (winner == false)
-    {
-        cout << "Game Tied!" << endl
-            << gameBoard[0][0] << " | " << gameBoard[0][1] << " | " << gameBoard[0][2] << endl;
-        cout << "__________" << endl;
-        cout << gameBoard[1][0] << " | " << gameBoard[1][1] << " | " << gameBoard[1][2] << endl;
-        cout << "__________" << endl;
-        cout << gameBoard[2][0] << " | " << gameBoard[2][1] << " | " << gameBoard[2][2] << endl;
-    }
-}
-
-if (winner == true)
-{
-    if (counter % 2 == 1)
-    {
-        cout << player1 << " WON!" << endl
-            << gameBoard[0][0] << " | " << gameBoard[0][1] << " | " << gameBoard[0][2] << endl;
-        cout << "__________" << endl;
-        cout << gameBoard[1][0] << " | " << gameBoard[1][1] << " | " << gameBoard[1][2] << endl;
-        cout << "__________" << endl;
-        cout << gameBoard[2][0] << " | " << gameBoard[2][1] << " | " << gameBoard[2][2] << endl;
-    }
-    if (counter % 2 == 0)
-    {
-        cout << player2 << " WON!" << endl
-            << gameBoard[0][0] << " | " << gameBoard[0][1] << " | " << gameBoard[0][2] << endl;
-        cout << "__________" << endl;
-        cout << gameBoard[1][0] << " | " << gameBoard[1][1] << " | " << gameBoard[1][2] << endl;
-        cout << "__________" << endl;
-        cout << gameBoard[2][0] << " | " << gameBoard[2][1] << " | " << gameBoard[2][2] << endl;
-    }
-}
-
-*/
